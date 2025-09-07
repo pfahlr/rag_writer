72687be05231334ef4900e65f41e6bd90c390ffe
Rick Pfahl <pfahlr@gmail.com>
2025-09-07 05:00:58 -0400

    Add MCP client helpers

--- src/tool/__init__.py
+++ src/tool/__init__.py
@@ -3,6 +3,7 @@
 from .base import Tool, ToolSpec, ToolRegistry
 from .agent import run_agent
 from .rag_tool import create_rag_retrieve_tool
+from .mcp_client import connect, fetch_tools, call_tool
 
 __all__ = [
     "Tool",
@@ -10,4 +11,7 @@ __all__ = [
     "ToolRegistry",
     "run_agent",
     "create_rag_retrieve_tool",
+    "connect",
+    "fetch_tools",
+    "call_tool",
 ]
